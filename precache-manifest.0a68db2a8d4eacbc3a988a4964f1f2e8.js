acc37c7ee0124c38c2b32ad95c08e6167f5c0754
griffinsandler <gsandler3@gatech.edu>
2022-08-03 16:47:38 -0400

    first commit

---
+++ precache-manifest.0a68db2a8d4eacbc3a988a4964f1f2e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0438dc17ba70a5f814ef90836386dd82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd29c2edb555667b4cd",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "3759454fa74936195555",
+    "url": "/static/css/main.f71643fb.chunk.css"
+  },
+  {
+    "revision": "cfd29c2edb555667b4cd",
+    "url": "/static/js/2.ce976f4b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.ce976f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3759454fa74936195555",
+    "url": "/static/js/main.13777499.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
